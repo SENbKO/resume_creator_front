4be6073b9c9ab0820de8c7d825cc030bda114f89
Arsen <anosovarsenij@gmail.com>
2025-11-02 20:13:12 +0100

    added description page

--- src/app/components/description/description.ts
+++ src/app/components/description/description.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InformationInterface } from '../../model/information-interface';
 
 @Component({
   selector: 'app-description',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './description.html',
   styleUrl: './description.css'
 })
-export class Description {
+export class Description  implements InformationInterface{
 
+  @Output() formValid = new EventEmitter<FormGroup>;
+
+  addInfo(): void {
+    const value = this.form.value.description;
+    localStorage.setItem('description', value!);
+    console.log(localStorage.getItem('description'));
+  }
+
+  form = new FormGroup(
+    {
+      description: new FormControl('', Validators.required)
+    }
+  )
+
+
+  ngOnInit(){
+    this.formValid.emit(this.form);
+  }
 }
--- src/app/pages/create-page/create-page.ts
+++ src/app/pages/create-page/create-page.ts
@@ -10,10 +10,11 @@ import { Resume } from '../../model/resume';
 import { ResumeService } from '../../services/resume-service';
 import { ModelExperience } from '../../model/experience';
 import { InformationInterface } from '../../model/information-interface';
+import { Description } from '../../components/description/description';
 
 @Component({
   selector: 'app-create-page',
-  imports: [ProgressBar, PersonalInformation, Experience],
+  imports: [ProgressBar, PersonalInformation, Experience, Description],
   templateUrl: './create-page.html',
   styleUrl: './create-page.css'
 })
@@ -29,6 +30,7 @@ export class CreatePage {
 
   @ViewChild(PersonalInformation) personalInformation !: InformationInterface;
   @ViewChild(Experience) experience !: InformationInterface;
+  @ViewChild(Description) description !: InformationInterface;
   currentForm : FormGroup | null = null;
   currentStepIndex = 0;
 
@@ -51,31 +53,12 @@ export class CreatePage {
       case 0: 
           this.personalInformation.addInfo();
           break;  
-      // const controls = this.personalInformation.mainInfoGroup.controls;
-      //         const personalInfo = new PersonalInfo(controls.firstName.value!, controls.secondName.value!, controls.email.value!, controls.position.value!);
-      //         localStorage.setItem("personalInfo", JSON.stringify(personalInfo));
       //         break;
-      case 1:  this.experience.addInfo();
-      //const experienceControls = this.experience.experiences;
-      //         console.log('adding experience');
-      //         let experiences : ModelExperience[] = [];
-      //         for(const control of experienceControls.controls){
-      //           const group = control as FormGroup;
-      //           const companyControl = group.get('company');
-      //           const positionControl = group.get('position');
-      //           const descriptionControl = group.get('description');
-              
-      //           if (companyControl && positionControl && descriptionControl) {
-      //             const experience: ModelExperience = new ModelExperience(
-      //               companyControl.value,
-      //               positionControl.value,
-      //               descriptionControl.value
-      //             );
-      //             experiences.push(experience);
-      //           }
-      //           localStorage.setItem('experiences', JSON.stringify(experiences));
-                
-      //         }
+      case 1: 
+          this.description.addInfo();
+          break;
+      case 2:  
+          this.experience.addInfo();
           break;
     }
   }
